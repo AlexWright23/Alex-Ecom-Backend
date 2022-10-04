dc8ec2947515359f1d5e3996aec021866ce09d2a
AlexWright23 <honeybadger937@gmail.com>
2022-10-04 19:47:35 -0400

    restarted multiple times, pushing what i have should be finished

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,15 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
@@ -14,7 +23,8 @@ Tag.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-  }
+  },
+  
 );
 
 module.exports = Tag;
